cf5baa01b8d605a6f8c0513e61634f53f800a5d2
poojabhosale971@gmail.com <poojabhosale971@gmail.com>
2023-11-27 16:35:07 +0530

    Add project files.

---
+++ Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        int a = 10;
+        public void Test()
+        {
+            Console.WriteLine("this is test method");
+        }
+
+        public void Getdata(string a,string b)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Test();
+            p.Getdata("Pooja","Pune");
+
+        }
+    }
+}
